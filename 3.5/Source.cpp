978efca3a8f24955b3c6c16a4518d409f4463286
Bashka <Bashka@BASHKAPC>
2017-10-18 01:36:24 +0300

    добавил программу проверяющую повторяющиеся слова

---
+++ 3.5/Source.cpp
@@ -0,0 +1,16 @@
+// proverka pobtoryushihsy slov-grammatiki
+#define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS
+#include "std_lib_facilities.h" 
+
+int main()
+{
+	string previous = " "; 
+
+	string current;
+	while (cin >> current) {
+		if (previous == current)
+
+		cout << "povtor clova: " << current << '\n';
+		previous = current;
+	}
+}
